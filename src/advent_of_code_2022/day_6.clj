c68214e66d3eb04e19b4a9cf289b5c0a75769a80
Sundeep Malladi <sundeep@malladi.io>
2023-03-12 21:18:44 -0500

    day 6; part 2

--- src/advent_of_code_2022/day_6.clj
+++ src/advent_of_code_2022/day_6.clj
@@ -1,11 +1,13 @@
 (ns advent-of-code-2022.day-5
   (:require [clojure.string :as str]))
 
+(def marker-size 14)
+
 (defn input->chunks [in]
   "Accepts a string and partitions it into quad char chunks"
   (map
-    (fn [index] (subs in index (+ index 4)))
-    (range 0 (- (+ 1 (count in)) 4))))
+    (fn [index] (subs in index (+ index marker-size)))
+    (range 0 (- (+ 1 (count in)) marker-size))))
 
 (defn sopm? [chunk]
   (= (count chunk) (count (set chunk))))
@@ -13,7 +15,7 @@
 (def puzzle-input (slurp "./src/puzzle-inputs/day-6.txt"))
 
 (def sopm (first (filter sopm? (input->chunks puzzle-input))))
-(+ 4 (str/index-of puzzle-input sopm))
+(+ marker-size (str/index-of puzzle-input sopm))
 
 
 
