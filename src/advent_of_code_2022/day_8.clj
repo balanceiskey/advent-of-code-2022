9b01916dfa8845d3dbd1e5a5345e99c33cca5692
Sundeep Malladi <sundeep@sproutsocial.com>
2023-03-29 09:08:48 -0500

    8:2

--- src/advent_of_code_2022/day_8.clj
+++ src/advent_of_code_2022/day_8.clj
@@ -19,13 +19,6 @@
 (def num-rows (count tree-map))
 (def num-columns (count (first tree-map)))
 
-(defn num-edge-trees
-  "Accepts a tree map and returns the number trees that make up its edge."
-  [tmap]
-  (let [width (count (get tmap 0 ))
-        height (count tmap)]
-    (+ (- (* 2 width) 2) (- (* 2 height) 2))))
-
 (defn get-in-tmap [tmap coords]
   (get-in tmap coords))
 
@@ -65,7 +58,28 @@
         col (range num-columns)]
     [row col]))
 
+; eval part 1
 (count (filter true? (map #(some-visible? % tree-map) (gen-coords num-rows num-columns))))
+
+(defn take-while-greater [coords tmap take-fn]
+  (let [value (get-in-tmap tmap coords)
+        taken (take-fn coords tmap)
+        stopper (or (first (filter #(>= % value) taken)) 10)
+        taken-whilst (take-while #(< % stopper) taken)]
+    (filter #(not= 10 %) (conj taken-whilst stopper))))
+
+
+(defn scenic-score [coords tmap]
+  (apply * (map count [(take-while-greater coords tmap take-top)
+                       (take-while-greater coords tmap take-left)
+                       (take-while-greater coords tmap take-right)
+                       (take-while-greater coords tmap take-bottom)])))
+
+(scenic-score [1 2] tree-map)
+(scenic-score [3 2] tree-map)
+
+; eval part 2
+(apply max (map #(scenic-score % tree-map) (gen-coords num-rows num-columns)))
 
 
 
